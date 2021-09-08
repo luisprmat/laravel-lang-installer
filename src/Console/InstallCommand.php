6d0b4995c47e307fa9950960ae70f36cba43bc08
Luis Antonio Parrado <luisprmat@gmail.com>
2021-09-07 23:32:06 -0500

    Add support for any language, no defualt option

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -7,38 +7,45 @@ use Illuminate\Filesystem\Filesystem;
 
 class InstallCommand extends Command
 {
-    protected $signature = 'lang:add-spanish {--I|inline : install validation.php with generic attributes (no name for attribute)}';
+    protected $signature = 'lang:add {locale=es : The language that should be installed (es, fr, pt, ...)}
+                                     {--I|inline : Install validation.php with generic attributes (no name for attribute)}
+                                     {--D|no-def : It does not change the default language in config/app.php}';
 
-    protected $description = 'Install translations for spanish language';
+    protected $description = "Install translations for language 'locale' (default 'es')";
 
     public function handle()
     {
-        (new Filesystem)->ensureDirectoryExists(resource_path('lang/es'));
+        $locale = (string)$this->argument('locale');
 
-        copy(__DIR__.'/../../vendor/laravel-lang/lang/locales/es/auth.php', resource_path('lang/es/auth.php'));
-        copy(__DIR__.'/../../vendor/laravel-lang/lang/locales/es/pagination.php', resource_path('lang/es/pagination.php'));
-        copy(__DIR__.'/../../vendor/laravel-lang/lang/locales/es/passwords.php', resource_path('lang/es/passwords.php'));
+        (new Filesystem)->ensureDirectoryExists(resource_path("lang/{$locale}"));
+
+        copy(__DIR__ . "/../../vendor/laravel-lang/lang/locales/{$locale}/auth.php", resource_path("lang/{$locale}/auth.php"));
+        copy(__DIR__ . "/../../vendor/laravel-lang/lang/locales/{$locale}/pagination.php", resource_path("lang/{$locale}/pagination.php"));
+        copy(__DIR__ . "/../../vendor/laravel-lang/lang/locales/{$locale}/passwords.php", resource_path("lang/{$locale}/passwords.php"));
 
         if ($this->option('inline')) {
-            copy(__DIR__.'/../../vendor/laravel-lang/lang/locales/es/validation-inline.php', resource_path('lang/es/validation.php'));
+            copy(__DIR__ . "/../../vendor/laravel-lang/lang/locales/{$locale}/validation-inline.php", resource_path("lang/{$locale}/validation.php"));
         } else {
-            copy(__DIR__.'/../../vendor/laravel-lang/lang/locales/es/validation.php', resource_path('lang/es/validation.php'));
+            copy(__DIR__ . "/../../vendor/laravel-lang/lang/locales/{$locale}/validation.php", resource_path("lang/{$locale}/validation.php"));
         }
 
-        $this->loadJsonFile();
+        $this->loadJsonFile($locale);
 
-        // Set config('app.locale') to 'es'
-        $this->pregReplaceInFile("/('locale')\s{0,}(=>)\s{0,}(')[A-Za-z_-]+(')\s{0,}(,)/", "'locale' => 'es',", config_path('app.php'));
-
-        $this->info('Language spanish installed successful');
+        if (!$this->option('no-def')) {
+            // Set config('app.locale')
+            $this->pregReplaceInFile("/('locale')\s{0,}(=>)\s{0,}(')[A-Za-z_-]+(')\s{0,}(,)/", "'locale' => '{$locale}',", config_path('app.php'));
+            $this->info("Language [{$locale}] installed successfully as default.");
+        } else {
+            $this->info("Language [{$locale}] installed successfully, but it isn't the default language.");
+        }
     }
 
     /**
      * Replace a given string within a given file matching with a regular expression.
      *
-     * @param  string  $search // (RegExp)
-     * @param  string  $replace
-     * @param  string  $path
+     * @param string $search // (RegExp)
+     * @param string $replace
+     * @param string $path
      * @return void
      */
     protected function pregReplaceInFile($search, $replace, $path)
@@ -46,8 +53,8 @@ class InstallCommand extends Command
         file_put_contents($path, preg_replace($search, $replace, file_get_contents($path)));
     }
 
-    private function loadJsonFile()
+    private function loadJsonFile($locale)
     {
-        copy(__DIR__.'/../../vendor/laravel-lang/lang/locales/es/es.json', resource_path('lang/es.json'));
+        copy(__DIR__ . "/../../vendor/laravel-lang/lang/locales/{$locale}/{$locale}.json", resource_path("lang/{$locale}.json"));
     }
 }
--- tests/Unit/PublishLanguageFilesTest.php
+++ tests/Unit/PublishLanguageFilesTest.php
@@ -9,9 +9,13 @@ class PublishLanguageFilesTest extends TestCase
     /** @test */
     function command_publishes_language_files()
     {
-        $this->artisan('lang:add-spanish')
-            ->expectsOutput('Language spanish installed successful');
+        $this->artisan('lang:add es')
+            ->expectsOutput("Language [es] installed successfully as default.");
 
-        $this->assertTrue(true);
+        $this->artisan('lang:add fr')
+            ->expectsOutput("Language [fr] installed successfully as default.");
+
+        $this->artisan('lang:add fr --no-def')
+            ->expectsOutput("Language [fr] installed successfully, but it isn't the default language.");
     }
 }
